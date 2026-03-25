5c70c3231c19ca262d83fd655360f21694965b60
sharath pillai <spillai.developer@gmail.com>
2026-03-25 09:36:22 +0530

    VITE backend url modified

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const downArrowBtn = document.querySelector(".down-arrow");
 const chatForm = document.querySelector(".chat-form");
 
 // Now, the frontend talks securely to our own backend server instead of directly to Google!
-const API_URL = `${process.env.Backend_URL}/api/chat`;
+const API_URL = `${import.meta.env.VITE_Backend_URL}/api/chat`;
 
 const userData = {
   message: null,
